9890dfa1d914b43953176b355f92769a59a2e2c3
steven <11642329+evorts@users.noreply.github.com>
2021-06-02 12:21:16 +0700

    forgot password

--- pkg/mailer/gmail.go
+++ pkg/mailer/gmail.go
@@ -47,7 +47,7 @@ func (g *gmailManager) SendHtml(ctx context.Context, targets []Target, subject, 
 	from := fmt.Sprintf("From: %s\n", g.senderEmail)
 	to := fmt.Sprintf("To: %s\n", strings.Join(tos, ","))
 	subject = fmt.Sprintf("Subject: %s\n", subject)
-	mime := fmt.Sprintf("MIME Version: 1.0; \nContent-Type: text/plain; charset=utf-8;\n\n")
+	mime := fmt.Sprintf("MIME Version: 1.0; \nContent-Type: text/html; charset=utf-8;\n\n")
 	t, err := g.tpl.Parse(html)
 	if err != nil {
 		return nil, err
